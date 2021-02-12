fa2e9b39ba8d490e6e38e88b324e9edac03ea17c
dsrikanth776 <75516370+dsrikanth776@users.noreply.github.com>
2021-02-12 16:48:47 +0530

    Add files via upload

---
+++ class4(1).js
@@ -0,0 +1,310 @@
+function customer(){
+
+    var whereustudy="saveetha"
+
+    var course="javascipt"
+
+    var percentage="70%"
+
+    var fee=2000
+    if(whereustudy=="saveetha")
+
+    {
+
+    console.log("yes im study ")
+
+    return
+
+    }
+
+    var joincourse="javascript"
+
+    var emis=1000
+
+    console.log("no worng")
+
+    return
+
+    }
+    customer()
+
+    function customer1(){
+
+    var whereustudy="saveetha"
+
+    var course="javascipt"
+
+    var percentage="70%"
+
+    var fee=2000
+
+    
+
+    if(whereustudy=="zph")
+
+    {
+
+        console.log("yes im study ")
+
+    return
+
+    }
+
+    var joincourse="javascript"
+
+    var emis=1000
+
+    console.log("no worng")
+
+    return
+
+    }
+
+    
+
+    customer1()
+
+    function car(){
+
+    var model="Hyundai"
+
+    var design="grand i10"
+
+    var price=600000
+
+    if(design=="grand i10")
+
+    {
+
+        console.log("this is new model car")
+
+    return
+
+    }
+
+    var likethis="yes i like this car"
+
+    var money=500000
+
+    console.log("discont price")
+
+    return
+
+    }
+
+    car()
+
+    console.log("<br>");
+
+    console.log("<br>");
+
+    function car1(){
+
+    var model="Hyundai"
+
+    var design="grand i10"
+
+    var price=600000
+
+    if(design=="yyyyi10")
+
+    {
+
+        console.log("this is new model car")
+
+    return
+
+    }
+
+    var likethis="yes i like this car"
+
+    var money=500000
+
+    console.log("discont price")
+
+    return
+
+    }
+
+    car1()
+      function bharath(){
+
+    var whereru="chennai"
+
+    var ruworking="yes"
+
+    var companyname="rbss"
+
+    var ctc=srikanth(whereru,ruworking,companyname,ctc)
+
+    console.log(ctc)
+
+    }
+
+    function srikanth(whereru,ruworking,companyname,ctc){
+
+    if(whereru=="chennai"){
+
+        console.log("im in chennai");
+
+        console.log("<br>");
+
+    return 240000
+
+    }
+
+    else  
+
+    {
+
+        console.log("dont come")
+
+    }
+
+    }
+
+    bharath()
+
+    console.log("<br>")
+
+    console.log("<br>")
+
+    
+
+    function bharathnaidu(){
+
+    var whereru="chennai"
+
+    var ruworking="yes"
+
+    var companyname="rbss"
+
+    var ctc=srikanth1(whereru,ruworking,companyname,ctc)
+
+    console.log(ctc)
+
+    }
+
+    function srikanth1(whereru,ruworking,companyname,ctc){
+
+    if(ruworking=="yes"){
+
+        console.log("yes im working in chennai");
+
+        console.log("<br>");
+
+    return 240000
+
+    }
+
+    else  
+
+    {
+
+        console.log("im not working")
+
+    }
+
+    }
+
+    bharathnaidu()
+
+    console.log("<br>")
+
+    console.log("<br>")
+
+    var bharath=["bng","yesworking",24000];
+
+    bharath[0]="chennai"
+
+    console.log(bharath);
+
+    console.log("<br>")
+
+    console.log("<br>")
+
+    
+
+    var person={firstname:"sreekanth",lastname:"dabbara",age:22};
+
+    firstname="sreekanth"
+
+    console.log(firstname);
+
+    console.log("<br>")
+
+    console.log("<br>")
+
+    var friuts=["banana","apple","orangre","mango"];
+
+    console.log(friuts)
+
+    console.log("<br>")
+
+    
+
+    console.log(friuts.length)
+
+    friuts[0]="grapes"
+
+    console.log("<br>")
+
+    
+
+    console.log(friuts)
+
+    console.log("<br>")
+
+    var person=[];
+
+    person[0]="sreekanth"
+
+    person[1]="dabbara"
+
+    person[2]=22
+
+    console.log(person[0]+" "+person[1]+" "+person[2]+" ");
+
+    console.log("<br>")
+
+    console.log(person.length);
+
+    console.log("<br>")
+
+    console.log("<hr>")
+
+    
+
+    //there useing popping is remove the last element
+
+    var mobiles=["apple","vivo","redme","realme"];
+
+    console.log(mobiles);
+
+    console.log("<br>")
+
+    //its pop working
+
+    mobiles.pop()
+
+    console.log(mobiles);
+
+    console.log("<br>")
+
+    //there useing pushing is add a new   element
+
+    var mobiles=["apple","vivo","redme","realme"];
+
+    console.log(mobiles);
+
+    console.log("<br>")
+
+    //its push working
+
+    mobiles.push("oppo")
+
+    console.log(mobiles);
+
+    console.log("<hr>")
+
+    console.log("<br>")
